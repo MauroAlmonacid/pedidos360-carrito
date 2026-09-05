a84a8a74861a7b4c1d30f74411f133810746a0a8
MauroAlmonacid <canepheler@gmail.com>
2026-09-04 22:38:05 -0400

    V1.0.0: Creacion de microservicio Carrito

---
+++ src/main/java/cl/duoc/pedidos360/pedidos360_carrito/Pedidos360CarritoApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.pedidos360.pedidos360_carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pedidos360CarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pedidos360CarritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/pedidos360/pedidos360_carrito/Pedidos360CarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.pedidos360.pedidos360_carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pedidos360CarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
